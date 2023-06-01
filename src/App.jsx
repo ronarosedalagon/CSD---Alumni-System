2151b97423e890d1f4d3455a81aebb783e210d4d
ronarosedalagon <ronagin114@gmail.com>
2023-06-02 07:07:58 +0800

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Welcome to your first hosting!!!
+      </div>
+      <p className="read-the-docs">
+        nya mag unsa naman ta ani ron?
+      </p>
+    </>
+  )
+}
+
+export default App
